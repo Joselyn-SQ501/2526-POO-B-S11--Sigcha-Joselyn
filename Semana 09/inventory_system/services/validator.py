c762273aa466b98babac89d3e2150bb963bdc818
Joselyn Sigcha <ja.sigchaq@uea.edu.ec>
2026-02-14 18:29:07 -0500

    Tarea S09 - Actualización de la clase de servicio de validación de la carpeta services del Sistema de Gestión de inventarios 📄

--- Semana 09/inventory_system/services/validator.py
+++ Semana 09/inventory_system/services/validator.py
@@ -1,10 +1,14 @@
+# Clase de servicio que valida las entradas del usuario
 class Validator:
+
+    # Método estático que valida que el ID del producto no esté vacío, retornando un mensaje de error si la validación falla
     @staticmethod
     def validate_id(id_value):
         if not id_value or len(id_value.strip()) == 0:
             return False, "✖️ El ID no puede estar vacío"
         return True, ""
     
+    # Método estático que valida que el nombre del producto no esté vacío y tenga al menos 2 caracteres, retornando un mensaje de error si la validación falla
     @staticmethod
     def validate_name(name):
         if not name or len(name.strip()) == 0:
@@ -12,7 +16,8 @@ class Validator:
         if len(name.strip()) < 2:
             return False, "El nombre debe tener al menos 2 caracteres"
         return True, ""
-
+    
+    # Método estático que valida que el valor ingresado sea un número entero válido y no negativo, retornando un mensaje de error si la validación falla
     @staticmethod
     def validate_int(value):
         try:
@@ -22,7 +27,8 @@ class Validator:
             return True, ""
         except ValueError:
             return False, "⚙️Debe ingresar un número entero válido"
-
+    
+    # Método estático que valida que el valor ingresado sea un número decimal válido y no negativo, retornando un mensaje de error si la validación falla
     @staticmethod
     def validate_float(value):
         try:
@@ -31,4 +37,4 @@ class Validator:
                 return False, "✖️El valor no puede ser negativo"
             return True, ""
         except ValueError:
-            return False, "Debe ingresar un número decimal válido"
+            return False, "⚙️Debe ingresar un número decimal válido"
