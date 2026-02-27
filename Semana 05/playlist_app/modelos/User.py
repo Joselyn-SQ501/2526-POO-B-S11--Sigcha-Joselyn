4b6c820bebe3386161d4729be80e1eb263214489
Joselyn Sigcha <ja.sigchaq@uea.edu.ec>
2026-02-27 14:35:02 -0500

    Proyecto Sistema de Gestión de Inventarios necesario para la tarea de la semana 11

--- Semana 05/playlist_app/modelos/User.py
+++
@@ -1,5 +0,0 @@
-# Subclase para ingresar datos sobre el usuario
-class User:
-    # Creación de un constructor vacío
-    def __init__(self):
-        self.user_type = "" # Atributo para ingresar el tipo de usuario como tipo de dato string.
--- Semana 08/Dashboard_Organization_POO/model/MenuModel.py
+++
@@ -1,8 +0,0 @@
-# Creación de una clase para manejar el menú de unidades
-class MenuModel:
-    # Constructor vacío que inicializa las unidades disponibles
-    def __init__(self):
-        self.unidades = {
-            '1': 'Unidad 1', # Uso como atributo de la carpeta unidad 1 del proyecto POO, como parte de la modularización del archivo Dashboard.py
-            '2': 'Unidad 2' # Uso como atributo de la carpeta unidad 2 del proyecto POO, como parte de la modularización del archivo Dashboard.py
-        }
