f5ed1cb242a212b1e61fcb4e5d06a537529a02e5
Joselyn Sigcha <ja.sigchaq@uea.edu.ec>
2026-02-27 23:59:32 -0500

    Tarea S11 - Actualización de la clase de servicio de validador de la carpeta services del Sistema de Gestión de inventarios 📄

--- Semana 11/improved_inventory_system/services/validator.py
+++ Semana 11/improved_inventory_system/services/validator.py
@@ -5,14 +5,14 @@ class Validator:
     @staticmethod
     def validate_id(id_value):
         if not id_value or len(id_value.strip()) == 0:
-            return False, "✖️ El ID no puede estar vacío"
+            return False, "El ID no puede estar vacío"
         return True, ""
     
     # Método estático que valida que el nombre del producto no esté vacío y tenga al menos 2 caracteres, retornando un mensaje de error si la validación falla
     @staticmethod
     def validate_name(name):
         if not name or len(name.strip()) == 0:
-            return False, "✖️ El nombre no puede estar vacío"
+            return False, "El nombre no puede estar vacío"
         if len(name.strip()) < 2:
             return False, "El nombre debe tener al menos 2 caracteres"
         return True, ""
@@ -23,10 +23,10 @@ class Validator:
         try:
             number = int(value)
             if number < 0:
-                return False, "✖️El valor no puede ser negativo"
+                return False, "El valor no puede ser negativo"
             return True, ""
         except ValueError:
-            return False, "⚙️Debe ingresar un número entero válido"
+            return False, "Debe ingresar un número entero válido"
     
     # Método estático que valida que el valor ingresado sea un número decimal válido y no negativo, retornando un mensaje de error si la validación falla
     @staticmethod
@@ -34,7 +34,7 @@ class Validator:
         try:
             number = float(value)
             if number < 0:
-                return False, "✖️El valor no puede ser negativo"
+                return False, "El valor no puede ser negativo"
             return True, ""
         except ValueError:
-            return False, "⚙️Debe ingresar un número decimal válido"
+            return False, "Debe ingresar un número decimal válido"
